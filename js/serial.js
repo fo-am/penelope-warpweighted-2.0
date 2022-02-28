3757c636cd485a8f62d76630cb05e4f0a7c6ee3b
dave griffiths <dave@fo.am>
2022-02-28 12:05:11 +0000

    working 5x5 version

--- js/serial.js
+++ js/serial.js
@@ -1,37 +1,81 @@
 var port;
 var lineBuffer = '';
 
+const filters = [
+  // Filter on devices with the Arduino Uno USB Vendor/Product IDs.
+	{ usbVendorId: 0x2341, usbProductId: 0x0043 },
+	{ usbVendorId: 0x2341, usbProductId: 0x0001 },
+	{ usbVendorId: 0x1a86, usbProductId: 0x7523 },
+];
+
+var pm_s = [0,0,0,0,0,
+			0,0,0,0,0,
+			0,0,0,0,0,
+			0,0,0,0,0,
+			0,0,0,0,0];
+
 async function getReader() {
-	var port = await navigator.serial.requestPort({});
-	await port.open({ baudRate: 9600 });
-	const appendStream = new WritableStream({
-		write(chunk) {
-			lineBuffer += chunk;
-			var lines = lineBuffer.split('n');				
-			while (lines.length > 1) {
-				var message=lines.shift();
-				console.log(message);
-				document.getElementById("info").innerHTML=message;
+	var port = await navigator.serial.requestPort({ filters });
+
+	try {
+		await port.open({ baudRate: 9600 });
+		const reader = port.readable.getReader();
+		// Listen to data coming from the serial device.
+		while (true) {
+			const { value, done } = await reader.read();
+			if (done) {
+				// Allow the serial port to be closed later.
+				reader.releaseLock();
+				break;
 			}
-			lineBuffer = lines.pop();
+			// value is a Uint8Array.
+			update_from_pm(String.fromCharCode.apply(null, value).split("\r\n"));
 		}
-	});
-	port.readable
-		.pipeThrough(new TextDecoderStream())
-		.pipeTo(appendStream);	
+	}
+	catch (err) {
+		window.alert(err.message);
+	}
+	
+	
 }
 
-var str55="1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1";
-var str44="1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1";
-function listSerial(){
+function update_from_pm(codes) {
 
-	
-	
-	write_draft_interface( [[1,0,0,0],
-							[0,1,0,0],
-							[0,0,1,0],
-							[0,0,0,1]]);
-	
+	let trigger = false;
+	for (code of codes) {
+		let decoded = code.split(":")
+		if (decoded.length==2) {
+			let index=parseInt(decoded[0]);
+			let value=parseInt(decoded[1]);
+
+			if (index>=0 && index<25 && value>=0 && value<16) {
+				// seems valid
+				let b=0;
+				if (value!=0) b=1;
+				if (pm_s[index]!=b) {
+					trigger=true;
+					pm_s[index]=b;
+					console.log(code)
+				}
+			}
+		}
+	}
+
+	if (trigger) {
+		write_draft_interface([
+
+			[pm_s[20],pm_s[15],pm_s[10],pm_s[5],pm_s[0]],
+			[pm_s[21],pm_s[16],pm_s[11],pm_s[6],pm_s[1]],
+			[pm_s[22],pm_s[17],pm_s[12],pm_s[7],pm_s[2]],
+			[pm_s[23],pm_s[18],pm_s[13],pm_s[8],pm_s[3]],
+			[pm_s[24],pm_s[19],pm_s[14],pm_s[9],pm_s[4]],
+
+		]);
+		
+	}	
+}
+
+function listSerial(){	
 	if (port) {
 		port.close();
 		port = undefined;
